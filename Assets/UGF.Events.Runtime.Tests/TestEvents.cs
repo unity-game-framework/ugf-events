d1e3b648e1e5edb372bb8d38e75eb5d40b49e3d7
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-16 15:54:43 +0100

    Add tests

--- Assets/UGF.Events.Runtime.Tests/TestEvents.cs
+++ Assets/UGF.Events.Runtime.Tests/TestEvents.cs
@@ -1,9 +1,14 @@
 ﻿using NUnit.Framework;
+using Unity.Profiling;
 
 namespace UGF.Events.Runtime.Tests
 {
     public class TestEvents
     {
+        private ProfilerMarker m_marker = new ProfilerMarker("TestEvents.InvokeMultiple");
+        private ProfilerMarker m_marker2 = new ProfilerMarker("TestEvents.InvokeMultiple2");
+        private Event m_event = new EventList();
+
         private class Target
         {
             public int Count { get; private set; }
@@ -25,6 +30,23 @@ namespace UGF.Events.Runtime.Tests
             event0.Invoke((10, 10.5F));
 
             Assert.AreEqual(1, target.Count);
+        }
+
+        [Test]
+        public void InvokeMultiple()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                m_event.Add(() => { });
+            }
+
+            m_marker.Begin();
+            m_event.Invoke();
+            m_marker.End();
+
+            m_marker2.Begin();
+            m_event.Invoke();
+            m_marker2.End();
         }
     }
 }
